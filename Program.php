6503e4204895893e24ace2d1c9b083993bf0457a
Caio Henrique <henriquerocha.dev@gmail.com>
2023-11-19 00:35:08 -0300

    print and modifications
    .

--- Program.php
+++ Program.php
@@ -1,8 +1,13 @@
 <?php 
 include_once 'Back.php';
 
-$estacionamento1 = new Estacionamento("Modelo 1", "ABC-1234", "09:00", "18:00", 0);
+$estacionamento1 = new Estacionamento("FIAT", "ABB123", "05:00", "18:00", 0);
 $estacionamento1->setValorApagar();
 
+$estacionamento2 = new Estacionamento("FORD", "2XXZ23", "10:00", "22:00", 0);
+$estacionamento2->setValorApagar();
+
 echo $estacionamento1;
+echo "<br>";
+echo $estacionamento2;
 ?>
---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Sistema de Estacionamento PHP
+
+
+
+## Screenshots
+
+<picture><img src="./Print.png"></picture>
+
